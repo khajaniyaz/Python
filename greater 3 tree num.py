d4a1fe60fa63d3d910fdc601435eb31c58479144
khajaniyaz <khajaniyaz666@gmail.com>
2025-05-03 21:26:39 +0530

    Create greater 3 tree num.py

---
+++ greater 3 tree num.py
@@ -0,0 +1,13 @@
+num1=10
+num2=20
+num3=30
+if(num1>num2):
+    if(num1>num3):
+       print("num1 is graeter")
+    else:
+       print("num3 is greater")
+elif(num2>num3):
+    print("num2 is graeter")
+else:
+    print("num3 is greater")
+
